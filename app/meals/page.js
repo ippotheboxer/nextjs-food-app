1d4368bc837e35503192fb5c93a158643735c3b2
Sophie <sophiesokol123@gmail.com>
2025-04-09 15:09:24 +0100

    Add db with meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,8 +2,10 @@ import React from 'react';
 import classes from "./page.module.css";
 import Link from 'next/link';
 import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
 
-const MealsPage = () => {
+async function MealsPage() {
+  const meals = await getMeals();
   return (
     <>
       <header className={classes.header}>
@@ -18,7 +20,7 @@ const MealsPage = () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]}/>
+        <MealsGrid meals={meals}/>
       </main>
     </>
   );
